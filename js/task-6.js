3f1e7c711b70cfd95703f441c6cb158ee9bcd321
Sviatoslav Godyk <godyk1992@gmail.com>
2025-02-02 21:37:41 +0800

    changed mistakes in task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,36 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector(".js-controls");
 const inputEl = document.querySelector(".js-input");
 const createBtn = document.querySelector(".js-createbtn");
 const destroyBtn = document.querySelector(".js-destroybtn");
+const containerForBoxes = document.querySelector(".js-boxes");
 
-const startNum = 30;
-const startMargin = "10px";
+function createBoxes(amount) {
+  destroyBoxes();
 
-createBtn.addEventListener(`click`, (event) => {
-  event.preventDefault();
+  const boxes = [];
+  let size = 30;
 
-  const inputValue = inputEl.value;
-  let inputNumber;
-  if (!isNaN(inputValue) && inputValue.trim().length !== 0) {
-    inputNumber = parseInt(inputValue);
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.margin = `5px`;
+    box.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    boxes.push(box);
+  }
+  containerForBoxes.append(...boxes);
+}
+
+function destroyBoxes() {
+  containerForBoxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(inputEl.value);
+  if (amount >= 0 && amount <= 100) {
+    createBoxes(amount);
+    inputEl.value = "";
   } else {
-    alert("Incorrect input");
-  }
-  for (let i = 1; i <= inputNumber; i++) {
-    const div = document.createElement("div");
-    const size = startNum + (i - 1) * 10;
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-    div.style.backgroundColor = getRandomHexColor();
-    div.style.margin = startMargin;
-    div.classList.add("div-container");
-    document.body.appendChild(div);
+    alert("Please, enter a number between 1 and 100");
   }
 });
 
-destroyBtn.addEventListener("click", () => {
-  const divContainers = document.querySelectorAll(".div-container");
-  divContainers.forEach((div) => div.remove());
-});
+destroyBtn.addEventListener("click", destroyBoxes);
